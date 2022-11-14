0c440d0621bd46c7d3723e0ada1d31e8c464d22c
ibniyamine-chabane <ibni-yamine.chabane@laplateforme.io>
2022-11-14 15:16:22 +0100

    job03 terminer

---
+++ jour01/job03/index.php
@@ -0,0 +1,39 @@
+<?php
+
+$my_Boolean = true;
+$my_Integer = 6;
+$my_String = 'Salut';
+$my_float = 5.2 ;
+
+echo "<table>
+        <thead>
+            <tr>
+                <th>Type</th>
+                <th>Nom</th>
+                <th>Valeur</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th>boolean</th>
+                <td>my_Boolean</td>
+                <td>$my_Boolean</td>
+            </tr>
+            <tr>
+                <th>Integer</th>
+                <td>my_Integer</td>
+                <td>$my_Integer</td>
+            </tr>
+            <tr>
+                <th>string</th>
+                <td>my_string</td>
+                <th>$my_String</th>
+            </tr>
+            <tr>
+                <th>float</th>
+                <td>my_float</td>
+                <td>$my_float</td>
+            </tr>
+        </tbody>    
+     </table>"
+?>

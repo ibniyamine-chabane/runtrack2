01fda490913ab2d6412d0508e04ea36cc365033d
ibniyamine-chabane <ibni-yamine.chabane@laplateforme.io>
2022-11-16 17:09:04 +0100

    index.php dans le job04 fait et marche

---
+++ jour03/job04/index.php
@@ -0,0 +1,31 @@
+<?php 
+
+/*Créez une variable de type string nommée $str et affectez y le texte :
+“Dans l'espace, personne ne vous entend crier”.
+Créez un algorithme qui parcourt, compte et affiche le nombre de caractères présents
+dans cette chaîne.
+*/
+
+$str = "Dans l'espace, personne ne vous entend crier";
+
+$i = 0;
+$count = 0;
+    while (isset($str[$i])){
+        if (isset($str[$i]) == 1) {
+        $count = $count + 1;
+    }
+        $i++;
+        
+    }
+/*
+
+ou bien 
+
+while (isset($str[$i])){ 
+    $i++
+}
+
+echo $i;
+
+*/
+?>

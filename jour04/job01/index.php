87cbafb48a74a78030bbb4ebd0ce70b7789de599
ibniyamine-chabane <ibni-yamine.chabane@laplateforme.io>
2022-11-17 16:13:38 +0100

    job1 du jours 4 er 5 Fait

--- jour04/job01/index.php
+++ jour04/job01/index.php
@@ -1,31 +1,28 @@
 <?php 
+/*Développez un algorithme qui affiche le nombre d’arguments $_GET.
+Tips :
+Pour tester votre code, créez un formulaire html <form> de type GET avec différents
+champs <input> de type “text” et un <input> de type “submit” pour l’envoi.
+Vous pouvez ensuite afficher avec echo directement dans votre page par exemple :
+“Le nombre d’argument GET envoyé est : “*/
 
-$contenu = $_GET;
 $i = 0;
-
-while (isset($contenu[$i])){ 
-    $i++;
-}
-
-/*foreach ($contenu as $arg) {
-    echo "$contenu[$i] => $arg";
-    $i++;
-}
-    
-*/
-
-
-    echo "<form method='get'>
-            <label for='nom'>Quel est votre nom :<br/></label>
-            <input type='text' name='nom' id='nom'>
-            <label for='nom'><br/>Quel est votre prenom : <br/></label>
-            <input type='text' name='prenom' id='prenom'>
-            <label for='nom'><br/>Quel est votre age : <br/></label>
-            <input type='text' name='age' id='age'><br/>
-            <input type='submit' value='Envoyer'>
-        </form>";
-
-        echo 'Le nombre d’argument GET envoyé est :'.$i;
+   foreach ( $_GET as $value ) {
+            $i++;
+        }
         
-        var_dump($contenu);
+                
+      echo "<form method='get'>
+                <label for='nom'>Quel est votre nom :<br/></label>
+                <input type='text' name='nom' id='nom'>
+                <label for='nom'><br/>Quel est votre prenom : <br/></label>
+                <input type='text' name='prenom' id='prenom'>
+                <label for='nom'><br/>Quel est votre age : <br/></label>
+                <input type='text' name='age' id='age'><br/>
+                <input type='submit' value='Envoyer'>
+            </form>";
+
+        echo "Le nombre d’argument GET envoyé est : $i";
+        
+       //var_dump($_GET);
 ?>

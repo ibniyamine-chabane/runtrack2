56085e33b40623df2f0384d5d386d2c0a72df4e3
ibniyamine-chabane <ibni-yamine.chabane@laplateforme.io>
2022-11-18 09:57:11 +0100

    Job04 du jour04 fait et fonctionne

--- jour04/job04/index.php
+++ jour04/job04/index.php
@@ -1,44 +1,48 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 
-/*Développer un algorithme qui affiche dans un tableau HTML <table> l’ensemble des
-arguments $_ POST et les valeurs associées. Il doit y avoir deux colonnes : argument et
-valeur.
-Tips :
-Pour tester votre code, créez un formulaire html <form> de type POST avec différents
-champs <input> de type “text” et un <input> de type “submit” pour l’envoi.*/
+    <!--Développer un algorithme qui affiche dans un tableau HTML <table> l’ensemble des
+    arguments $_ POST et les valeurs associées. Il doit y avoir deux colonnes : argument et
+    valeur.
+    Tips :
+    Pour tester votre code, créez un formulaire html <form> de type POST avec différents
+    champs <input> de type “text” et un <input> de type “submit” pour l’envoi.-->
 
-foreach ( $_POST as $key => $value) {
-        echo $key.'<br/>';
-        }
+<form method='post'>
+    <label for='nom'>Quel est votre nom :<br/></label>
+    <input type='text' name='nom' id='nom'>
+    <label for='nom'><br/>Quel est votre prenom : <br/></label>
+    <input type='text' name='prenom' id='prenom'>
+    <input type='submit' value='Envoyer'>
+</form>
 
+<table style='border: 1px solid'>
+    <thead>
+        <th style='border: 1px solid'>arguments</th>
+        <th style='border: 1px solid'>valeur</th>
+    </thead>
+    <tbody>
+        <?php 
+            foreach ( $_POST as $key => $value) { 
+                echo "<tr>
+                        <td style='border:1px solid'>$key</td>
+                        <td style='border:1px solid'>$value</td>
+                     </tr>";
+             }    //boucle pour créer la ligne de la clé et valeur selon la longeur du tableau ici c'est nom et prenom.
+             var_dump($_POST);  //Je laisse le var_dump pour avoir une vu sur le contenu de $_POST.
+        ?>
+    </tbody>
+</table>
 
-        echo  "<form method='post'>
-                  <label for='nom'>Quel est votre nom :<br/></label>
-                  <input type='text' name='nom' id='nom'>
-                  <label for='nom'><br/>Quel est votre prenom : <br/></label>
-                  <input type='text' name='prenom' id='prenom'>
-                  <input type='submit' value='Envoyer'>
-              </form>";
-
-           
-
-
-      echo "<table style='border: 1px solid'>
-              <thead>
-                  <th style='border: 1px solid'>arguments</th>
-                  <th style='border: 1px solid'>valeur</th>
-              </thead>
-              <tbody>"
-              foreach ( $_POST as $key => $value) {
-                echo '<tr>
-                        <td>'.$key'</td>;
-                        <td>'.$value'</td>; //boucle pour créer la ligne de la clé et valeur
-                    </tr>'
-              }
-              "</tbody>
-          </table>";
-
-          var_dump($_POST);
+</body>
+</html>
 
 
-?>
+

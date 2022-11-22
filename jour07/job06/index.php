a13a20924514b62c1598db8fbe29bf3690acb2d6
ibniyamine-chabane <ibni-yamine.chabane@laplateforme.io>
2022-11-22 16:35:15 +0100

    jour07 job06 fait et marche

---
+++ jour07/job06/index.php
@@ -0,0 +1,46 @@
+<?php 
+
+/*Créez une fonction nommée “leetSpeak($str)”. Cette fonction prend en paramètre une
+chaîne de caractères nommée “$str”.
+Elle doit retourner la chaîne de caractères “$str” convertie en leet speak. Cela signifie
+qu’elle doit la modifier de sorte à ce que :
+● les “A” deviennent des “4”,
+● les “B” des “8”,
+● les “E” des “3”,
+● les “G” des “6”,
+● les “L” des “1”,
+● les “S” des “5”
+● les “T” des “7”.
+Cela est valable que l’on crie ou non (majuscules et minuscules).*/
+
+function leetSpeak($str) {
+
+    for ($i=0; isset($str[$i]) ; $i++) { 
+        if ( $str[$i] == "A" || $str[$i] == "a" ) {
+            $str[$i] = "4";
+        }
+        elseif ( $str[$i] == "B" || $str[$i] == "b" ) {
+            $str[$i] = "8";
+        }
+        elseif ( $str[$i] == "E" || $str[$i] == "e" ) {
+            $str[$i] = "3";
+        }
+        elseif ( $str[$i] == "G" || $str[$i] == "g" ) {
+            $str[$i] = "6";
+        }
+        elseif ( $str[$i] == "L" || $str[$i] == "l" ) {
+            $str[$i] = "1";
+        }
+        elseif ( $str[$i] == "S" || $str[$i] == "s" ) {
+            $str[$i] = "5";
+        }
+        elseif ( $str[$i] == "T" || $str[$i] == "t" ) {
+            $str[$i] = "7";
+        }                    
+    }
+    return $str;
+}
+
+echo leetSpeak("I Can only Count to Four, united state of gorgio giovana");
+
+?>
